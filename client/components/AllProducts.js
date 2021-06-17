c9693b5e2158e60a97f359145250c1c4bc2d3af9
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-06-17 11:44:13 -0500

    FE: changed 'isAdmin' to be 'isLoggedIn' functionality temporarily just to prevent bugs

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -59,7 +59,7 @@ class AllProducts extends React.Component {
 const mapState = (state) => {
   return {
     products: state.products,
-    isAdmin: state.user.isAdmin
+    isAdmin: !!state.auth.id
   };
 };
 
