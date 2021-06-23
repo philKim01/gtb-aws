36193961de251cb9877d605b08b3672a7ab6be84
Freddie Bergener <freddiembergener@gmail.com>
2021-06-23 11:44:18 -0400

    Feat: Added user confirmation component

--- client/Routes.js
+++ client/Routes.js
@@ -1,25 +1,26 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import Home from './components/Home';
-import {me} from './store/redux/auth'
-import AllProducts from './components/AllProducts'
-import SingleProduct from './components/SingleProduct'
-import Cart from './components/Cart';
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Orders from './components/Orders';
-import GuestCheckout from './components/GuestCheckout';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import { me } from "./store/redux/auth";
+import AllProducts from "./components/AllProducts";
+import SingleProduct from "./components/SingleProduct";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Orders from "./components/Orders";
+import GuestCheckout from "./components/GuestCheckout";
+import UserOrderConfirmation from "./components/UserOrderConfirmation";
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData()
+    this.props.loadInitialData();
   }
 
   render() {
-    const {isLoggedIn} = this.props
+    const { isLoggedIn } = this.props;
 
     return (
       <div>
@@ -34,39 +35,43 @@ class Routes extends Component {
           </Switch>
         ) : (
           <Switch>
-            <Route path='/' exact component={ Login } />
+            <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route exact path="/products" component={AllProducts} />
             <Route path="/products/:id" component={SingleProduct} />
             <Route exact path="/cart" component={Cart} />
-            <Route path='/guestcheckout' component={GuestCheckout} />
+            <Route path="/guestcheckout" component={GuestCheckout} />
+            <Route
+              path="/userorderconfirmation"
+              component={UserOrderConfirmation}
+            />
           </Switch>
         )}
       </div>
-    )
+    );
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+      dispatch(me());
+    },
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes));
---
+++ client/components/UserOrderConfirmation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserOrderConfirmation = () => {
+  return <h1>User Order Confirmation</h1>;
+};
+
+export default UserOrderConfirmation;
